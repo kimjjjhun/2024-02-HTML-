4188a961c428616b274d87bd9dd498a7dd17058d
kimjjjjhun <kjhun2727@gmail.com>
2024-03-15 16:47:22 +0900

    test

--- teamproject/login.js
+++ teamproject/login.js
@@ -13,4 +13,30 @@ signUpBtn.addEventListener("click", () => {
 });
 
 fistForm.addEventListener("submit", (e) => e.preventDefault());
-secondForm.addEventListener("submit", (e) => e.preventDefault());
+secondForm.addEventListener("submit", (e) => e.preventDefault()); 
+
+function check(){
+  if(document.frm.id.value == ""){
+    alert("아이디를 입력해주세요")
+    document.frm.id.focus();
+    return false;
+  }else if(document.frm.pass.value == ""){
+  alert("비밀번호를 입력해주세요")
+    document.frm.pass.focus();
+    return false;
+  }else if(isNaN(document.frm.pass.value)){
+      alert("정수만 입력해주세요!")
+      document.frm.pass.focus();
+      return false;
+  }else if(document.frm.id.value.length <= 3 ){
+      alert("아이디는 4자 이상 입력해주세요")
+      document.frm.id.focus();
+      return false;
+  }else if(document.frm.pass.value.length <= 3){
+    alert("비밀번호는 4자 이상 입력해주세요")
+        document.frm.pass.focus();
+        return false;
+    }else{
+      return true;
+    }
+ }
